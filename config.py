4c7c42104a5f4e0c53b87c768562b9f32016623a
TheVicio <thevicio27@gmail.com>
2020-08-19 13:45:54 -0300

    modo slowmode implementado!

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-token = 'NzMwNTE2NTI1Mjk1MDc1MzI4.XwYohw.-5Ee9Vm76VEWTfVBotfNGdgkbG0'
+token = 'NzMwNTE2NTI1Mjk1MDc1MzI4.XwYohw.I5eOHmcWhLZxf0mX869xBgU5s0A'
 delay = 10
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import discord
 import config
+import asyncio
+from datetime import datetime, timedelta
 
 class Bot(discord.Client):
     def __init__(self):
         super().__init__()
         
+        who_cant_say = dict()
+        
         @self.event
-        def on_ready():
-            print('Bot inicializado')
-            
-        @self.event
-        def on_message(message):
-            pass
+        async def on_message(message: discord.Message):
+            if isinstance(message.channel, discord.TextChannel):
+                if not message.author.name in who_cant_say:
+                    who_cant_say[message.author.name] = datetime.now() + timedelta(seconds = config.delay)
+                else:
+                    if datetime.now() < who_cant_say[message.author.name]:
+                        await message.delete()
+                        DMchannel = await message.author.create_dm()
+                        await DMchannel.send(f'O canal está em modo slow mode, você precisa esperar {config.delay} segundos para digitar novamente')
+                    else:
+                        del who_cant_say[message.author.name]
         
         self.run(config.token)
     
+Bot()
